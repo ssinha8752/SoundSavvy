99c32892546c73ed7b18254460d582e133fefa0c
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-04-10 02:48:02 +0530

    [Shubham][10-Apr] SoundSavvy - Adding context & reducer

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { CartCard } from '../components/CartCard';
 import headphonesData from '../Data.json'
 import { useTitle } from '../hooks/useTitle';
+import { useCart } from '../context/CartContext';
 
 export const Cart = () => {
 
+  const {cartList,total}=useCart();
   useTitle("Cart");
 
   return (
     <main>
       <section className="cart">
-        <h1 className="items">Cart Items: {headphonesData.length}</h1>
+        <h1 className="items">Cart Items: {cartList.length} / ${total}</h1>
         {headphonesData.map(headphone => (
             <CartCard key={headphone.id} headphone={headphone}/>
         ))}        
